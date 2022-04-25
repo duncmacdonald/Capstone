b3ec29520985f0685f73f9a92b6d7b10d5cde261
Duncan Macdonald <dunc.macdonald@gmail.com>
2022-04-24 20:55:30 -0700

    User's items are saved to SessionData cart

---
+++ src/Components/AddToCart/AddToCart.js
@@ -0,0 +1,36 @@
+import {
+  Close,
+  AddCircleOutline,
+  RemoveCircleOutline,
+} from "@mui/icons-material";
+import "./AddToCart.css";
+
+export default function AddToCart({ id, name, description, quantity, price, listener }) {
+  return (
+    <div className="overlay">
+      <div className="AddToCart">
+        <div className="AddToCart__Top">
+          <Close onClick={() => listener("close")}/>
+          <h1>{name}</h1>
+          <p>{description}</p>
+          {/* <input type={Text}></input> */}
+
+          <div className="AddToCart__Quantity">
+            {quantity <= 1 ? (
+              <RemoveCircleOutline htmlColor="#BFBFBF" />
+            ) : (
+              <RemoveCircleOutline onClick={() => listener("decrement")}/>
+            )}
+            <h1>{quantity}</h1>
+            <AddCircleOutline onClick={() => listener("increment")}/>
+          </div>
+        </div>
+        <div className="AddToCart__FooterButton" onClick={() => listener("save")}>
+          <div className="narrow"></div>
+          <h1>Add To Cart</h1>
+          <h1 className="narrow">${quantity * price}</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Pages/Menu/Menu.js
+++ src/Pages/Menu/Menu.js
@@ -1,14 +1,49 @@
 import React from "react";
 import "./Menu.css";
 import MenuItem from "../../Components/MenuItem/MenuItem";
+import AddToCart from "../../Components/AddToCart/AddToCart";
 
 export default class Menu extends React.Component {
   state = {
     restaurantId: this.props.match.params.restaurantId,
-    choice: undefined,
+    choice: "none",
+    quantity: 1,
   };
 
-  menuListener = (id) => this.setState({choice: id });
+  componentDidMount() {
+    console.log(sessionStorage.getItem("cart"));
+  }
+
+  menuListener = (id) => this.setState({ choice: id, quantity: 1 });
+
+  addToCartListener = (action) => {
+    console.log(action);
+    switch (action) {
+      case "increment":
+        this.setState({ quantity: this.state.quantity + 1 });
+        break;
+      case "decrement":
+        this.setState({ quantity: this.state.quantity - 1 });
+        break;
+      case "close":
+        this.setState({ choice: "none", quantity: 1 });
+        break;
+      case "save":
+        if (sessionStorage.getItem("cart") !== null) {
+          sessionStorage.setItem("cart", JSON.stringify([
+            ...JSON.parse(sessionStorage.getItem("cart")),
+            { itemId: this.state.choice, quantity: this.state.quantity },
+          ]));
+        } else {
+          sessionStorage.setItem("cart", JSON.stringify([
+            { itemId: this.state.choice, quantity: this.state.quantity },
+          ]));
+        }
+
+        this.setState({ choice: "none", quantity: 1 });
+        break;
+    }
+  };
 
   render() {
     return (
@@ -30,6 +65,17 @@ export default class Menu extends React.Component {
             listener={this.menuListener}
           />
         </ul>
+        {this.state.choice !== "none" ? (
+          <AddToCart
+            name={"Pasta"}
+            description={"noodles and sauce"}
+            price={8.99}
+            quantity={this.state.quantity}
+            listener={this.addToCartListener}
+          />
+        ) : (
+          <></>
+        )}
       </div>
     );
   }
